efa0686e9ab5f7517a36a60452539276503ad5e8
BisiDavi <oludavidconnect@gmail.com>
2022-11-08 22:35:00 +0100

    struct updated

--- structs/using-structs.rs
+++ structs/using-structs.rs
@@ -4,16 +4,32 @@ struct Rectangle{
     height:u32
 }
 
-fn main(){
-    let rect = Rectangle {width:40,height:60};
-    println!("The area of the triangle is {} square pixels", area(&rect));
-    println!("rect is {:#?}", rect);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
-fn area(rectangle:&Rectangle) -> u32{
-    rectangle.width * rectangle.height
+#[derive(Debug)]
+struct Circle{
+    radius:f64
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        self.radius * self.radius * (22.0 / 7.0)
+    }
+}
+
+fn main(){
+    let rect = Rectangle {width:40,height:60};
+    let circle = Circle {radius: 7.0};
+    println!("The area of the triangle is {:#?} square pixels", rect.area());
+    println!("The area of the circle is {} m2", circle.area());
+    println!("rect is {:#?}", rect);
 }
  
 
 //methods are defined within the context of a struct and their first parameter is always self.
 //The self represents the instance of the struct the method is being called on
+// methods helps for organization
