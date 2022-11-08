2ab5edce21521e51d9bde42b30e94cc86c5a2fdb
BisiDavi <oludavidconnect@gmail.com>
2022-11-08 14:47:33 +0100

    slice updated

--- ownership/slices.rs
+++ ownership/slices.rs
@@ -12,22 +12,24 @@ fn main(){
     let first_name = &user_name[0..7]; //using string slice
     let last_name = &user_name[8..user_name.len()]; 
     let slice = &user_name[..];
+    let bn = [1,2,3,4,5];
+    let bn_slice = &bn[1..4];
 
+    println!("bn_slice {:?}", bn_slice);
     println!("slice {}", slice);
     println!("first_name {}", first_name);
     println!("last_name {}", last_name);
 } 
 
 // return first word in a spaced string
-fn first_word(given_word: &String) -> usize  {
+fn first_word(given_word: &str) -> &str  {
     let word_array = given_word.as_bytes();
 
     for (i, &item) in word_array.iter().enumerate(){
-        println!("&item {}", item);
         if item == b' '{
-            return i;
+            return &given_word[0..i];
         }
     }
 
-    given_word.len()
+   &given_word[..]
 }
