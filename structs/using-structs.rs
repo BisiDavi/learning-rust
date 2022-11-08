cb4b0acfff0cda0da19650db98342b68fac44c46
BisiDavi <oludavidconnect@gmail.com>
2022-11-08 23:32:34 +0100

    structs updated

--- structs/using-structs.rs
+++ structs/using-structs.rs
@@ -5,11 +5,17 @@ struct Rectangle{
 }
 
 impl Rectangle {
+    //method
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    //method
     fn can_hold(&self, other:&Rectangle) -> bool{
         self.width > other.width && self.height > other.height
+    }
+    // associated function
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
     }
 }
 
@@ -25,10 +31,12 @@ impl Circle {
 }
 
 fn main(){
-    let rect_1 = Rectangle {width:40,height:60};
     let rect_2 = Rectangle {width:10,height:20};
+    let rect_1 = Rectangle {width:40,height:60};
+    let square_1 = Rectangle::square(4);
     let circle = Circle {radius: 7.0};
     println!("Can rect_1 hold rect_2? {}", rect_1.can_hold(&rect_2));
+    println!("square_1, {:?}", square_1);
     println!("The area of the rectangle is {:#?} square pixels", rect_1.area());
     println!("The area of the rectangle is {:#?} square pixels", rect_2.area());
     println!("The area of the circle is {} m2", circle.area());
