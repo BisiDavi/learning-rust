9b6b3e323ed467cb8fa9f15eab9dff3a31e42f49
BisiDavi <oludavidconnect@gmail.com>
2022-11-11 04:23:50 +0100

    mint added

---
+++ nft-contract/src/mint.rs
@@ -0,0 +1,9 @@
+use crate::*;
+
+#[near_bindgen]
+impl Contract{
+    #[payable]
+    pub fn nft_mint(&mut self, token_id:Option<TokenId>, metadata:TokenMetadata, receiver_id:Option<AccountId>){
+
+    }
+}
