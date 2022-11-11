b48d56a3a6c236df662b93457b4b4cc8e68ead27
BisiDavi <oludavidconnect@gmail.com>
2022-11-11 04:10:22 +0100

    lib updated

--- nft-contract/src/lib.rs
+++ nft-contract/src/lib.rs
@@ -1,3 +1,42 @@
+use std::collections::HashMap;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::json_types::{Base64VecU8, U128};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue};
+
+pub use crate::metadata::*;
+pub use crate::mint::*;
+pub use crate::nft_core::*;
+pub use crate::approval::*;
+pub use crate::royalty::*;
+
+mod approval;
+mod enumeration;
+mod metadata;
+mod mint;
+mod nft_core;
+mod royalty;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract{
+
+}
+
+#[derive(BorshSerialize)]
+pub enum StorageKey{
+    TokensPerOwner,
+    TokensPerOwnerInner {account_id_hash:CryptoHash},
+    TokensById,
+    TokenMetadataById,
+    NFTContractMetadata,
+    TokensPerType,
+    TokensPerTypeInner {token_type_hash: CryptoHash},
+    TokenTypesLocked
+}
+
+#[near_bindgen]
 impl Contract{
     /*
         initialization function can only be called once,
