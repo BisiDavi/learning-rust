130114b1d32ff1166afd165a4e10abb3d231b1d3
BisiDavi <oludavidconnect@gmail.com>
2022-11-11 02:56:33 +0100

    nft-contract added

--- enums/index.rs
+++ enums/index.rs
@@ -7,4 +7,21 @@ fn main(){
     enum IpAddrKind{
         V4, V6
     }
+
+    enum Message {
+        Quit,
+        Move {x:i32, y:i32},
+        Write(String),
+        ChangeColor(i32, i32, i32)
+    }
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
+
+fn route(ip_kind: IpAddrKind){
+    println!("Ip-Kind, {}", ip_kind);
 }
